8aa76297d7304d009e91a7a4ce9329f852a1862f
Manix <victor.moncada001@gmail.com>
2024-10-02 17:31:35 -0300

    Creación de carpetas, y primera clase

---
+++ clases/empledo.py
@@ -0,0 +1,11 @@
+class empleado():
+    def __init__(self,id_empleado,nombre,fecha_contrato,salario,correo,telefono,direccion,rut,fecha_nacimiento):
+        self.id_empleado = id_empleado
+        self.nombre = nombre
+        self.fecha_contrato = fecha_contrato
+        self.salario = salario
+        self.correo = correo
+        self.telefono = telefono
+        self.direccion = direccion
+        self.rut = rut
+        self.fecha_nacimiento = fecha_nacimiento
